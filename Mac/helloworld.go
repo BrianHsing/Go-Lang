ad85c8b8fe0263182c03e3dbe14c33f37e3d7a88
Brian Hsing <brian.hsing@kyndryl.com>
2022-03-24 15:57:49 +0800

    update

--- Mac/helloworld.go
+++ Mac/helloworld.go
@@ -5,6 +5,15 @@ import "fmt"
 var count int = 4
 var string1 string = "你好"
 
+type person struct {
+	name string
+	age  int
+}
+
 func main() {
-	fmt.Println("Hello World", string1)
+	var P person
+	P.name = "brian"
+	P.age = 30
+	fmt.Printf("%s is %d year old \n", P.name, P.age)
+
 }
